5e26699ffd715a0cd02e5c3ffa244f3a38a5b492
Ikhsan Ardi <ikhsan.ardi@silentmode.my>
2024-11-24 12:07:35 +0700

    add job detail & reactive state

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,8 @@
 <script setup>
 import JobListing from "./JobListing.vue";
-import jobData from "@/jobs.json";
-import { ref, defineProps } from "vue";
+import { reactive, ref, defineProps, onMounted } from "vue";
 import { RouterLink } from "vue-router";
+import axios from "axios";
 
 defineProps({
   limit: Number,
@@ -12,7 +12,22 @@ defineProps({
   },
 });
 
-const jobs = ref(jobData);
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get("http://localhost:5001/jobs");
+
+    state.jobs = response.data;
+  } catch (error) {
+    console.log("error fetching jobs", error);
+  } finally {
+    state.isLoading = false;
+  }
+});
 </script>
 
 <template>
@@ -24,7 +39,7 @@ const jobs = ref(jobData);
 
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
-          v-for="job in jobs.slice(0, limit || jobs.length)"
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
           :job="job"
         />
