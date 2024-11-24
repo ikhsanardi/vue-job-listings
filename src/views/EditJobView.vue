39c0c2c5047ae4b1cbc1ae577bd32dad329d16d0
Ikhsan Ardi <ikhsan.ardi@silentmode.my>
2024-11-24 17:41:41 +0700

    add edit page

--- src/views/EditJobView.vue
+++ src/views/EditJobView.vue
@@ -1,8 +1,13 @@
 <script setup>
 import router from "@/router";
 import axios from "axios";
-import { reactive } from "vue";
+import { reactive, onMounted } from "vue";
 import { useToast } from "vue-toastification";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const jobID = route.params.id;
 
 const form = reactive({
   type: "Full-Time",
@@ -18,10 +23,15 @@ const form = reactive({
   },
 });
 
+const state = reactive({
+  job: {},
+  isLoading: true,
+});
+
 const toast = useToast();
 
 const handleSubmit = async () => {
-  const newJob = {
+  const updatedJob = {
     type: form.type,
     title: form.title,
     description: form.description,
@@ -36,15 +46,37 @@ const handleSubmit = async () => {
   };
 
   try {
-    const response = await axios.post("/api/jobs", newJob);
+    const response = await axios.put(`/api/jobs/${jobID}`, updatedJob);
 
-    toast.success("Job created successfully");
+    toast.success("Job updated successfully");
     router.push(`/jobs/${response.data.id}`);
   } catch (error) {
-    console.error("error creating job", error);
-    toast.error("Error creating job");
+    console.error("error updating job", error);
+    toast.error("Error updating job");
   }
 };
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(`/api/jobs/${jobID}`);
+
+    state.job = response.data;
+    form.type = state.job.type;
+    form.title = state.job.title;
+    form.description = state.job.description;
+    form.salary = state.job.salary;
+    form.location = state.job.location;
+
+    form.company.name = state.job.company.name;
+    form.company.description = state.job.company.description;
+    form.company.contactEmail = state.job.company.contactEmail;
+    form.company.contactPhone = state.job.company.contactPhone;
+  } catch (error) {
+    console.log("error fetching job", error);
+  } finally {
+    state.isLoading = false;
+  }
+});
 </script>
 
 <template>
@@ -54,7 +86,7 @@ const handleSubmit = async () => {
         class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
       >
         <form @submit.prevent="handleSubmit">
-          <h2 class="text-3xl text-center font-semibold mb-6">Add Job</h2>
+          <h2 class="text-3xl text-center font-semibold mb-6">Edit Job</h2>
 
           <div class="mb-4">
             <label for="type" class="block text-gray-700 font-bold mb-2"
@@ -209,7 +241,7 @@ const handleSubmit = async () => {
               class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
               type="submit"
             >
-              Add Job
+              Update Job
             </button>
           </div>
         </form>
